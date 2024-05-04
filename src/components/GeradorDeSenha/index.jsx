44eab8bac3f3b03eee0bfda826a9da7ba8111846
Wessley Silva <wessley.silva@outlook.com.br>
2024-05-04 11:50:21 -0300

    fix password generator to use all char. Add copy function

--- src/components/GeradorDeSenha/index.jsx
+++ src/components/GeradorDeSenha/index.jsx
@@ -6,11 +6,9 @@ const senha = document.getElementById('senha')
 export default function GeradorDeSenha() {
     const [ textoCopiado, setTextoCopiado ] = useState('Copiar')
     const [senhaGerada, setSenhaGerada ] = useState('')
+    const [TextoGerarSenha, setTextoGerarSenha] = useState('Gerar senha')
+
     let senha = ''
-    function handleCopiar() {
-        setTextoCopiado("Copiado!")
-        // falta add codigo para ctrl-c
-    }
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -22,18 +20,27 @@ export default function GeradorDeSenha() {
         let char = '1234567890_)(*&¨%$#@!+/;.?:>qwerrtyuiopasdfghjklçzxcvbnm'
         let length = 15;
         for(let i =0; i < length; i++) {
-            let randInt = getRandomInt(0, length + 1)
+            let randInt = getRandomInt(0, char.length + 1)
             let randomChar = char[randInt]
             senha += randomChar
         }
         setSenhaGerada(senha) 
+        setTextoGerarSenha('Gerar nova senha')
+        setTextoCopiado('Copiar')
     }
+
+    function handleCopiar(e) {
+        setTextoCopiado("Copiado!")
+        e.preventDefault()
+        navigator.clipboard.writeText(senhaGerada)
+    }
+    
     return (
         <>
         <div className={styles.container}>
             <h1>Gerador de senhas</h1>
             <div className={styles.buttons}>
-                <button onClick={gerarSenha}>Gerar senha</button>
+                <button onClick={gerarSenha}>{TextoGerarSenha}</button>
                 <button onClick={handleCopiar}> {textoCopiado} </button>
             </div>
                 <p>{senhaGerada}</p>
