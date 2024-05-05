16351610a9ee243e7f588593622ee41c78296a4d
Wessley Silva <wessley.silva@outlook.com.br>
2024-05-05 20:06:58 -0300

    add error if none of char options checked, and password generated as char group checked

--- src/components/GeradorDeSenha/index.jsx
+++ src/components/GeradorDeSenha/index.jsx
@@ -8,8 +8,9 @@ export default function GeradorDeSenha() {
     const [senhaGerada, setSenhaGerada ] = useState('')
     const [TextoGerarSenha, setTextoGerarSenha] = useState('Gerar senha')
     const [hidden, setHidden] = useState(true)
-
+    const [errors, setErrors] = useState('')
     let senha = ''
+    let char = ''
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -18,11 +19,28 @@ export default function GeradorDeSenha() {
       }
 
     function gerarSenha() {
-        let char = '1234567890_)(*&¨%$#@!+/;.?:>qwerrtyuiopasdfghjklçzxcvbnm'
+        setErrors('')
         let letters = 'abcdefghijklmnopqrstuvwxyz'
         let numbers = '1234567890'
         let specials = '_)(*&¨%$#@!+/;.?:>'
         let length = 15;
+
+        if(letras.checked) {
+            char += letters
+        }
+
+        if(numeros.checked) {
+            char += numbers
+        }
+
+        if (especiais.checked) {
+            char += specials
+        }
+
+        if (!especiais.checked && !numeros.checked && !letras.checked) {
+            return setErrors('É necessário ao menos um tipo de caracter.')
+        }
+
         for(let i =0; i < length; i++) {
             let randInt = getRandomInt(0, char.length)
             let randomChar = char[randInt]
@@ -43,6 +61,8 @@ export default function GeradorDeSenha() {
         return setHidden(!hidden)
     }
 
+    
+
     return (
         <>
         <div className={styles.container}>
@@ -54,11 +74,12 @@ export default function GeradorDeSenha() {
             </div>
             <div className={`${styles.options} ${hidden && styles.hidden}`}>
                 <p>Opções de Caracteres:</p>
-                <div className={styles.inputs}><input type="checkbox" name="letras" id="letras" /> <label htmlFor="letras">Letras</label></div>
-                <div className={styles.inputs}><input type="checkbox" name="numeros" id="numeros" /> <label htmlFor="numeros">Números</label></div>
-                <div className={styles.inputs}><input type="checkbox" name="especiais" id="especiais" /> <label htmlFor="especiais">Caracteres Especiais</label></div>
+                <div className={styles.inputs}><input type="checkbox" name="letras" id="letras" defaultChecked /> <label htmlFor="letras" >Letras</label></div>
+                <div className={styles.inputs}><input type="checkbox" name="numeros" id="numeros" defaultChecked/> <label htmlFor="numeros">Números</label></div>
+                <div className={styles.inputs}><input type="checkbox" name="especiais" id="especiais" defaultChecked /> <label htmlFor="especiais">Caracteres Especiais</label></div>
             </div>
                 <p>{senhaGerada}</p>
+                <p id={styles.errors}>{errors}</p>
         </div>
         </>
     )
